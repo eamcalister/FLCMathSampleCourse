228c1b508c53511395ac2c895612e41099225561
eamcalister <mcalister_e@fortlewis.edu>
2025-06-30 17:01:30 -0600

    Got annotations functional after running prefig new.

--- output/activities-web/lunr-pretext-search-index.js
+++ output/activities-web/lunr-pretext-search-index.js
@@ -106,7 +106,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet",
   "number": "",
   "title": "Math 221 Tangent Line Worksheet",
-  "body": " Math 221 Tangent Line Worksheet     Show how to include an image using Prefigure code.    Show displayed and alligned math as well as some minor math typesetting issues that come from xml syntax (see the code for details).      In this activity students are asked to decide when to switch the tangent line approximation they use to approximate square roots.    Set-up  Recall from class that we can approximate square roots of numbers that aren't perfect squares using the tangent line approximation near some number  where .  For instance, to approximate we decided to use the tangent line approximation at the nearest perfect square, which is given by  Thus, we can estimate .  Now, what if there isn't a \"nearest\" perfect square? For instance, which tangent line should we use to approximate ( is exactly halfway between and )? The following exercise will help us figure out the answer.     The graph of , along with its tangent lines at and , is given by .   The graph of and its tangent line approximations at and .      a=4  b=9  f(x) = x^(0.5)          (4,2)    f(x) = \\sqrt{x}         (9,3)            Use to give intervals of -values on which and give more accurate estimates of . That is, find some -value, , such that whenever and whenever .      Use algebra to justify your answer above.      Interactive Remarks    Note the issues in the code where Latex syntax conflicted with xml. There are a few instances when these happen.      Prefigure is really more intuitive to use than Tikz and generates more accessible images. Full documentation is available at and a very convenient tool for testing your figures is available at .     "
+  "body": " Math 221 Tangent Line Worksheet     Show how to include an image using Prefigure code.    Show displayed and alligned math as well as some minor math typesetting issues that come from xml syntax (see the code for details).      In this activity students are asked to decide when to switch the tangent line approximation they use to approximate square roots.    Set-up  Recall from class that we can approximate square roots of numbers that aren't perfect squares using the tangent line approximation near some number  where .  For instance, to approximate we decided to use the tangent line approximation at the nearest perfect square, which is given by  Thus, we can estimate .  Now, what if there isn't a \"nearest\" perfect square? For instance, which tangent line should we use to approximate ( is exactly halfway between and )? The following exercise will help us figure out the answer.     The graph of , along with its tangent lines at and , is given by .   The graph of and its tangent line approximations at and .      a=4  b=9  f(x) = x^(0.5)          (4,2)    f(x) = \\sqrt{x}         (9,3)                   Use to give intervals of -values on which and give more accurate estimates of . That is, find some -value, , such that whenever and whenever .      Use algebra to justify your answer above.      Interactive Remarks    Note the issues in the code where Latex syntax conflicted with xml. There are a few instances when these happen.      Prefigure is really more intuitive to use than Tikz and generates more accessible images. Full documentation is available at and a very convenient tool for testing your figures is available at .     "
 },
 {
   "id": "sec-section-name-6-2",
@@ -124,7 +124,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "1",
   "title": "",
-  "body": "  The graph of , along with its tangent lines at and , is given by .   The graph of and its tangent line approximations at and .      a=4  b=9  f(x) = x^(0.5)          (4,2)    f(x) = \\sqrt{x}         (9,3)            Use to give intervals of -values on which and give more accurate estimates of . That is, find some -value, , such that whenever and whenever .      Use algebra to justify your answer above.    "
+  "body": "  The graph of , along with its tangent lines at and , is given by .   The graph of and its tangent line approximations at and .      a=4  b=9  f(x) = x^(0.5)          (4,2)    f(x) = \\sqrt{x}         (9,3)                   Use to give intervals of -values on which and give more accurate estimates of . That is, find some -value, , such that whenever and whenever .      Use algebra to justify your answer above.    "
 },
 {
   "id": "prefigure-math-remarks-2",
@@ -151,7 +151,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet",
   "number": "",
   "title": "Math 222 Series Test Worksheet",
-  "body": " Math 222 Series Test Worksheet     Demonstrate a variety of interactive exercises using Calculus II problems (for some reason).      If given as a pre-class activity or uncollected set of exercises in class (so printing would not be necessary), it might make sense to have interactive exercises that check answers and give some sort of feedback. When a print version of this worksheet is generated you get static versions of these problems.    True\/False  vector space   Suppose is a sequence such that . Can you conclude that the series converges?    Check your notes from last class to find a counterexample.    The fact that does not imply necessarily converges. You can apply the integral test to series.      Here's a MyOpenMath problem. They are very easy to include, but perhaps not that attractive.       Here is a Webwork problem, which are also easy to include. (Hoping that a new FLC Webwork server, which will more closely resemble what you see here, will be up by Fall.) Using variants of actual assigned Webwork problems can be helpful in building student confidence when they do their assigned homework. Students must click the \"Activate\" button for the exercise to be interactive.  It is also possible to author problems directly using pg code.     Compute the value of the following improper integral. If it is divergent, type \"Diverges\" or \"D\". Answer:  Using the improper integral from above and the Integral Test, determine whether the following series converges or diverges. Answer \"Converges\" or \"Diverges.\"  Answer:   Converges    Diverges                      Here is an example of a Parson's problem for a proof that a series converges. There are many problem types that can be authored  The problem is to decide if converges.     This series is of the form , where .    Since , this series converges.     , i.e. the sequence of absolute values is positive and decreasing.         Thus, this series converges by the Alternating Series Test.      "
+  "body": " Math 222 Series Test Worksheet     Demonstrate a variety of interactive exercises using Calculus II problems (for some reason).      If given as a pre-class activity or uncollected set of exercises in class (so printing would not be necessary), it might make sense to have interactive exercises that check answers and give some sort of feedback. When a print version of this worksheet is generated you get static versions of these problems.    True\/False  vector space   Suppose is a sequence such that . Can you conclude that the series converges?    Check your notes from last class to find a counterexample.    The fact that does not imply necessarily converges. You can apply the integral test to series.      Here's a MyOpenMath problem. They are very easy to include, but perhaps not that attractive.       Here is a Webwork problem, which are also easy to include. (Hoping that a new FLC Webwork server, which will more closely resemble what you see here, will be up by Fall.) Using variants of actual assigned Webwork problems can be helpful in building student confidence when they do their assigned homework. Students must click the \"Activate\" button for the exercise to be interactive.  It is also possible to author problems directly using pg code.     Compute the value of the following improper integral. If it is divergent, type \"Diverges\" or \"D\". Answer:  Using the improper integral from above and the Integral Test, determine whether the following series converges or diverges. Answer \"Converges\" or \"Diverges.\"  Answer:   Converges    Diverges                      Here is an example of a Parson's problem for a proof that a series converges. There are many problem types that can be authored  The problem is to decide if converges.      This series is of the form , where .    Since , this series converges.     , i.e. the sequence of absolute values is positive and decreasing.         Thus, this series converges by the Alternating Series Test.      Interactive Exercise Remarks and Recources   For more documentation on Webwork problems, see (link).   For a general source for sample code, including exercise (other than Webwork), see .   "
 },
 {
   "id": "sec-section-name-7-2",
@@ -196,7 +196,25 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "4",
   "title": "",
-  "body": "  Here is an example of a Parson's problem for a proof that a series converges. There are many problem types that can be authored  The problem is to decide if converges.     This series is of the form , where .    Since , this series converges.     , i.e. the sequence of absolute values is positive and decreasing.         Thus, this series converges by the Alternating Series Test.     "
+  "body": "  Here is an example of a Parson's problem for a proof that a series converges. There are many problem types that can be authored  The problem is to decide if converges.      This series is of the form , where .    Since , this series converges.     , i.e. the sequence of absolute values is positive and decreasing.         Thus, this series converges by the Alternating Series Test.    "
+},
+{
+  "id": "interactive-exercise-remarks-2",
+  "level": "2",
+  "url": "sec-section-name-7.html#interactive-exercise-remarks-2",
+  "type": "Remark",
+  "number": "10",
+  "title": "",
+  "body": " For more documentation on Webwork problems, see (link).  "
+},
+{
+  "id": "interactive-exercise-remarks-3",
+  "level": "2",
+  "url": "sec-section-name-7.html#interactive-exercise-remarks-3",
+  "type": "Remark",
+  "number": "11",
+  "title": "",
+  "body": "For a general source for sample code, including exercise (other than Webwork), see . "
 }
 ]
 
